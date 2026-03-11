aea42da15b3a31ae37ea854450b997baf14f60ce
mirko <mirko.viroli@unibo.it>
2026-03-11 22:55:25 +0100

    lesson 03: fp2

---
+++ src/test/scala/u03/BooleanLambdaTest.scala
@@ -0,0 +1,26 @@
+package u03
+
+import org.junit.*
+import org.junit.Assert.*
+
+class BooleanLambdaTest extends LambdaTest:
+  import Lambda.*
+
+  @Test
+  def testNot() =
+    assertEquals(Not(False), True)
+    assertEquals(Not(True), False)
+
+  @Test
+  def testOr() =
+    assertFalse(toBoolean(Or(False)(False)))
+    assertTrue(toBoolean(Or(False)(True)))
+    assertTrue(toBoolean(Or(True)(False)))
+    assertTrue(toBoolean(Or(True)(True)))
+
+  @Test
+  def testAnd() =
+    assertFalse(toBoolean(And(False)(False)))
+    assertFalse(toBoolean(And(False)(True)))
+    assertFalse(toBoolean(And(True)(False)))
+    assertTrue(toBoolean(And(True)(True)))
---
+++ src/test/scala/u03/LambdaTest.scala
@@ -0,0 +1,13 @@
+package u03
+
+
+abstract class LambdaTest:
+  import Lambda.*
+
+  def toBoolean(l: Lambda): Boolean =
+    val l1: Lambda = x => x
+    val l2: Lambda = y => y
+    l(l1)(l2) match
+      case res if res == l1 => true
+      case res if res == l2 => false
+      case _ => ??? // throws an Exception
